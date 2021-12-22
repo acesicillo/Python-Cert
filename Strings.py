def16f1cd22e3d1762ed9fdf99b05fa9cc349060
David Moreno C <acesicillo@users.noreply.github.com>
2021-12-22 08:14:08 -0600

    commit on master

---
+++ Strings.py
@@ -0,0 +1,14 @@
+str1 = "pass"
+str2 = "word"
+str3 = str1 + str2
+str4 = "ha"
+str5 = str4 * 4
+print(str3)
+print(str5)
+
+my_srt = "\"David Moreno\""
+print(my_srt)
+print(my_srt.lower())
+print(my_srt.upper())
+print(my_srt.find("M"))
+print(my_srt,my_srt)
